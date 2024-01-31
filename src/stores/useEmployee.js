ef1ab50eec2a5e911c883761f961e36c357f6c64
le2yeji <96720279+le2yeji@users.noreply.github.com>
2024-01-31 17:41:06 +0900

    Update useEmployee.js

--- src/stores/useEmployee.js
+++ src/stores/useEmployee.js
@@ -1,7 +1,8 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
 import { fetchWrapper } from '@/helpers';
 
-const baseUrl = `${import.meta.env.VITE_API_URL}/api/v1/employees`;
+//const baseUrl = `${import.meta.env.VITE_API_URL}/api/v1/employees`;
+const baseUrl = `/api/v1/employees`;
 
 export const useEmployeeStore = defineStore('employee', {
   state: () => ({
@@ -36,7 +37,8 @@ export const useEmployeeStore = defineStore('employee', {
     async deleteEmployee(id) {
       try {
         this.isLoading = true;
-        const deletedEmployee = await fetchWrapper.post(`${import.meta.env.VITE_API_URL}/api/v1/employee/${id}`);
+        //const deletedEmployee = await fetchWrapper.post(`${import.meta.env.VITE_API_URL}/api/v1/employee/${id}`);
+        const deletedEmployee = await fetchWrapper.post(`/api/v1/employee/${id}`);
         this.employees = this.employees.filter((t) => t.id !== deletedEmployee.id);
       } catch (error) {
         console.log(error);
